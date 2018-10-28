cf6074e0d1389bae8795f64f308736ede2775280
Jackson Taber <jr_taber@yahoo.com>
2018-10-28 16:29:59 -0400

    fixed form

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -7,31 +7,31 @@
     {!! Form::open(['action' => 'addCustomerController@store', 'method' => 'POST']) !!}
             <div class="form-group">
                 {{Form::label('CustomerName', 'Name')}}
-                {{Form::text('CustomerName', '')}}
+                {{Form::text('CustomerName', '', ['class' => 'form-control', 'placeholder' => ''])}}
             </div>
             <div class="form-group">
                     {{Form::label('CustomerAddress', 'Address')}}
-                    {{Form::text('CustomerAddress', '')}}
+                    {{Form::text('CustomerAddress', '', ['class' => 'form-control', 'placeholder' => ''])}}
             </div>
             <div class="form-group">
                     {{Form::label('City', 'City')}}
-                    {{Form::text('City', '')}}
+                    {{Form::text('City', '', ['class' => 'form-control', 'placeholder' => ''])}}
             </div>
             <div class="form-group">
                     {{Form::label('StateCode', 'State Code')}}
-                    {{Form::text('StateCode', '')}}
+                    {{Form::text('StateCode', '', ['class' => 'form-control', 'placeholder' => ''])}}
             </div>
             <div class="form-group">
                     {{Form::label('ZIP', 'ZIP')}}
-                    {{Form::text('ZIP', '')}}
+                    {{Form::text('ZIP', '', ['class' => 'form-control', 'placeholder' => ''])}}
             </div>
             <div class="form-group">
                     {{Form::label('Phone', 'Phone')}}
-                    {{Form::text('Phone', '')}}
+                    {{Form::text('Phone', '', ['class' => 'form-control', 'placeholder' => ''])}}
             </div>
             <div class="form-group">
                     {{Form::label('Email', 'Email')}}
-                    {{Form::text('Email', '')}}
+                    {{Form::text('Email', '', ['class' => 'form-control', 'placeholder' => ''])}}
             </div>
             {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
     {!! Form:: close() !!}
