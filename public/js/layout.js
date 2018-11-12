8f4be440d2908b80bae2f27f683e98bad5079e31
Jason VanHouter <16123901+jvanhouter@users.noreply.github.com>
2018-11-11 19:50:30 -0500

    Moved JS and CSS for layout into proper structure

--- public/js/layout.js
+++ public/js/layout.js
@@ -22,3 +22,20 @@ function searchFunction() {
 $(document).ready(function() {
     $('.form-select2').select2();
 });
+
+
+//------------------------------------------------------------------
+
+// Open/Close Nav for the Navbar
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
